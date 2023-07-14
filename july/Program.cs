37ad001aedb95f30773bf2b7ac5301775e2c1d62
АхетовА <@213-3>
2023-07-14 21:46:42 +0600

    Добавьте файлы проекта.

---
+++ july/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace july
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your name: ");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Hello, {input}");
+
+        }
+    }
+}
